433f2707578ed29a37c46a0a73b40a558d78d605
Maks Mikayloff <ksimmi@yandex.com>
2019-02-13 00:33:43 +0600

    Cкрипт развертывания пакета поставки адаптирован под sqlite.

--- scripts/apply-update.sh
+++ scripts/apply-update.sh
@@ -10,8 +10,6 @@ fi
 RBENV_PATH=${HOME}/.rbenv
 RBENV_BIN_PATH=${RBENV_PATH}/bin
 RBENV_SHIMS_PATH=${RBENV_PATH}/shims
-MYSQL_DB_NAME="todo_db"
-MYSQL_USER_NAME="todouser"
 
 PATH=${RBENV_BIN_PATH}:${RBENV_SHIMS_PATH}:$PATH
 
@@ -23,13 +21,20 @@ version_part=$(cat ${project_root}/VERSION)
 backup_date_part=$(date +"%Y-%m-%d--%H-%M")
 backup_dir_path=${project_root}_backup_${backup_date_part}_v${version_part}
 
+db_path=${project_root}/db/production.sqlite3
+stash_db_path=/tmp/db/production.sqlite3
+
 systemctl stop puma-${instance_name}.service
 
 su ${SUDO_USER} <<USERCOMMANDS
 PATH=${RBENV_BIN_PATH}:${RBENV_SHIMS_PATH}:$PATH
+
+echo " # Put db to temporary stash"
+cp ${db_path} ${stash_db_path}
+
 echo " # Creating backup"
 mv ${project_root} ${backup_dir_path}
-mysqldump -u${MYSQL_USER_NAME} ${MYSQL_DB_NAME} > ${backup_dir_path}
+mv ${db_path} ${backup_dir_path}
 
 echo " # Extracting app package"
 mkdir ${project_root}
@@ -45,7 +50,6 @@ USERCOMMANDS
 
 echo " # Restart services"
 systemctl start puma-${instance_name}.services
-systemctl restart nginx.services
 
 echo "# Clenup"
 rm ${HOME}/${package_name}
