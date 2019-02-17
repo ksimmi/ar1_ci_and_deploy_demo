71c871467df33c4d02988ff39ec0f8995225d62e
Maks Mikayloff <ksimmi@yandex.com>
2019-02-17 15:39:21 +0600

    Стабилизирую поставку: искал проблему не в том скрипте - отменил все последние правки

--- scripts/create-deployment-package.sh
+++ scripts/create-deployment-package.sh
@@ -4,19 +4,16 @@ set -e
 
 version=${1}
 
-#cd web-shop
+cd web-shop
 
 RAILS_ENV=production rake assets:precompile
 
 echo ${version} > VERSION
 
-script_execution_dir=$(pwd)
-echo Scripts runed under $script_execution_dir directory
+tar -czf ${HOME}/artifacts/web-shop-${build_number}.tar.gz \
+--exclude=README.md \
+--exclude=features \
+--exclude=test \
+.
 
-#tar -czf ${HOME}/artifacts/web-shop-${build_number}.tar.gz \
-#--exclude=README.md \
-#--exclude=features \
-#--exclude=test \
-#.
-
-#cd -
+cd -
