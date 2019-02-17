b87946e6ff85161e4278bd359d0d2168d71ac4a0
Maks Mikayloff <ksimmi@yandex.com>
2019-02-17 12:45:15 +0600

    Стабилизирую поставку: вывожу информацию о пути в котором выполняется скрипт.

--- scripts/create-deployment-package.sh
+++ scripts/create-deployment-package.sh
@@ -10,6 +10,9 @@ RAILS_ENV=production rake assets:precompile
 
 echo ${version} > VERSION
 
+script_execution_dir=$(pwd)
+echo "Scripts runed under ${script_execution_dir} directory"
+
 tar -czf ${HOME}/artifacts/web-shop-${build_number}.tar.gz \
 --exclude=README.md \
 --exclude=features \
