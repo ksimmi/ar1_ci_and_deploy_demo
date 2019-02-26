542a3ae624599bd5812ee2b3ec12ac11776e8680
Maks Mikayloff <ksimmi@yandex.com>
2019-02-27 03:54:57 +0600

    Стабилизирую поставку: дублирую код из provision.sh в apply-update.sh, т.к. не получается иначе пуму перезапустить

--- scripts/apply-update.sh
+++ scripts/apply-update.sh
@@ -53,6 +53,24 @@ bundle install
 
 echo " # Configuring app"
 rake db:migrate
+
+cat > ${project_root}/config/environments/${instance_name}.rb <<EOL
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_files = false
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  config.log_level = :info
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
+end
+EOL
 USERCOMMANDS
 
 cat > /etc/systemd/system/puma-${instance_name}.service <<EOL
